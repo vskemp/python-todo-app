d1a3c3da58587966f9a72a90da66f67756a2eb3c
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2019-11-13 14:46:18 -0500

    implements delete_todo from todo function

--- to_do.py
+++ to_do.py
@@ -1,1 +1,19 @@
-# ITS TODO TIME
+# KEEP TRACK OF TODOS USING A LIST
+todo_list = []
+
+# I need a way to add todos
+def add_todo(todo):
+    # we recieve a todo, which is a string
+    todo_list.append(todo)
+
+# print the empty todo list
+print(todo_list)
+add_todo("feed the cat")
+# print todo_list again to make sure our todo got added
+print(todo_list)
+
+# I need to be able to delete todos
+
+# I need to print my todos
+
+# Show user the main menu
