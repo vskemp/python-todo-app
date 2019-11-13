1d923ab3aac9ade5aac34f1636337a680e08a339
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2019-11-13 15:25:01 -0500

    implements print_todos

--- to_do.py
+++ to_do.py
@@ -1,16 +1,22 @@
 # KEEP TRACK OF TODOS USING A LIST
 todo_list = []
 
+# I need to print my todos
+def print_todos():
+    for todo in todo_list:
+        print(todo)
+
 # I need a way to add todos
 def add_todo(todo):
     # we recieve a todo, which is a string
     todo_list.append(todo)
 
 # print the empty todo list
-print(todo_list)
+print_todos()
+#add todos by calling our function
 add_todo("feed the cat")
 # print todo_list again to make sure our todo got added
-print(todo_list)
+print_todos()
 
 # I need to be able to delete todos
 def delete_todo(index):
@@ -20,10 +26,8 @@ def delete_todo(index):
     except IndexError:
         print("🔥Sorry, there is nothing to remove here!🔥")
 delete_todo(0)
-print(todo_list)
+print_todos()
 delete_todo(0)
-print(todo_list)
-
-# I need to print my todos
+print_todos()
 
 # Show user the main menu
