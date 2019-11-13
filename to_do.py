3bc4ca91b6f1b63560ea2346a22e99270c73fecc
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2019-11-13 15:16:34 -0500

    implements delete_todo from todo function

--- to_do.py
+++ to_do.py
@@ -13,6 +13,16 @@ add_todo("feed the cat")
 print(todo_list)
 
 # I need to be able to delete todos
+def delete_todo(index):
+    # del todo_list[index]
+    try:
+        todo_list.pop(index)
+    except IndexError:
+        print("🔥Sorry, there is nothing to remove here!🔥")
+delete_todo(0)
+print(todo_list)
+delete_todo(0)
+print(todo_list)
 
 # I need to print my todos
 
